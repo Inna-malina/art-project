14191015887b995e8758083b0ea349a5e7972b10
Inna Cherkashina <atljhbyfbyyf@mail.ru>
2024-02-27 21:45:57 +0700

    add script for slider of bunner

--- js/index.js
+++ js/index.js
@@ -23,3 +23,44 @@ navClose.addEventListener('click', function (elem) {
   navBarContent.classList.remove('navbar-move');
 });
 // выезжающее бургер-меню конец
+
+//слайдер в баннере начало
+let bunners = document.querySelectorAll('.bunner-bg');
+let btn_R = document.querySelector('.bunner-arrow__right');
+let btn_L = document.querySelector('.bunner-arrow__left');
+
+let count = 0
+function slider(){
+  for (let n = 0; n < bunners.length; n++) {
+    bunners[n].classList.add('bg-disactive');
+  } 
+  bunners[count].classList.remove('bg-disactive');
+  bunners[count].classList.add('bg-active');
+}
+slider(); 
+
+btn_R.addEventListener('click', function (elem) {
+  elem.preventDefault();
+  if (count + 1 == bunners.length) {
+    count = 0;
+  }
+  else {
+    count++;
+  }
+  slider();
+});
+
+btn_L.addEventListener('click', function (elem) { 
+  elem.preventDefault();
+  if (count - 1 == -1) {
+    count = bunners.length - 1;
+  }
+  else {
+    count--;
+  }
+  slider();
+})
+
+
+
+//слайдер в баннере конец
