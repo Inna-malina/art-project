f46e17e4da2de3bad3df67ef9f21cef121abcf91
Inna Cherkashina <atljhbyfbyyf@mail.ru>
2024-03-03 21:27:22 +0700

    add scripts to tochangetab.js and infplist.js

--- dist/index.js
+++ dist/index.js
@@ -13,29 +13,41 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function toChangeTab() {
+  //Функция табов
   let navElements = document.querySelectorAll('.nav-tab');
   let tabImgContainer = document.querySelector('.tab-images__container img');
+  let tabImg_1 = document.querySelector('.tab-img-1 img');
+  let tabImg_2 = document.querySelector('.tab-img-2 img');
+  let tabImg_3 = document.querySelector('.tab-img-3 img');
+  let tabImg_4 = document.querySelector('.tab-img-4 img');
+  let tabText_1 = document.querySelector('.tab-text-1');
+  let tabText_2 = document.querySelector('.tab-text-2');
+  let tabText_3 = document.querySelector('.tab-text-3');
+  let tabText_4 = document.querySelector('.tab-text-4');
   
-
-
-  navElements.forEach(function (navElem) {
-  
+  navElements.forEach(function (navElem) {  
     navElem.addEventListener('click', function (event) {
       let action = event.target;
       disabledNav(navElements)
       action.classList.add('active-nav'); 
 
       let elementNav = _infoList__WEBPACK_IMPORTED_MODULE_0__["default"][action.dataset.nav];
-      let info = Object.values(elementNav)[0];
-      tabImgContainer.src = info.background;
-      
+      let info = Object.values(elementNav);
+      console.log(info[0]);
+      tabImgContainer.src = info[0];
+      tabImg_1.src = info[1];
+      tabImg_2.src = info[3];
+      tabImg_3.src = info[5];
+      tabImg_4.src = info[7];
+      tabText_1.textContent = info[2];
+      tabText_2.textContent = info[4];
+      tabText_3.textContent = info[6];
+      tabText_4.textContent = info[8];
     });
 
   });
 
-
-
-
+//Сброс стилей у неактивных табов при клике
   function disabledNav(blocks) {
     blocks.forEach(function (el) {
       el.classList.remove('active-nav');
@@ -52,59 +64,59 @@ function toChangeTab() {
 __webpack_require__.r(__webpack_exports__);
 const infoList = {
   "one-nav": {
-    "background": "../images/main/simpler/nav-1.png",
+    "background": "./images/main/simpler/nav-1.png",
     "oneNavImg1": "./images/main/simpler/foto-1.png",
-    "one-nav__text-1": "Портрет рисует художник.Детально прорисовывает все части портрета! ",
-    "one-nav_img-2": "/images/main/simpler/foto-2.png",
-    "one-nav__text-2": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем. ",
-    "one-nav_img-3": "/images/main/simpler/foto-3.png",
-    "one-nav__text-3": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",
-    "one-nav_img-4": "/images/main/simpler/foto-4.png",
-    "one-nav__text-4": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
+    "oneNavText1": "Портрет рисует художник.Детально прорисовывает все части портрета! ",
+    "oneNavImg2": "/images/main/simpler/foto-2.png",
+    "oneNavText2": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем. ",
+    "oneNavImg3": "/images/main/simpler/foto-3.png",
+    "oneNavText3": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",
+    "oneNavImg4": "/images/main/simpler/foto-4.png",
+    "oneNavText4": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
   },
   "two-nav": {
-    "background": "",
+    "background": "./images/main/simpler/nav-2.png",
     "oneNavImg1": "./images/main/simpler/foto-4.png",
-    "one-nav__text-1": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем.",
-    "one-nav_img-2": "/images/main/simpler/foto-3.png",
-    "one-nav__text-2": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",
-    "one-nav_img-3": "/images/main/simpler/foto-2.png",
-    "one-nav__text-3": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
-    "one-nav_img-4": "/images/main/simpler/foto-1.png",
-    "one-nav__text-4": "Портрет рисует художник.Детально прорисовывает все части портрета!",
+    "oneNavText1": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем.",
+    "oneNavImg2": "/images/main/simpler/foto-3.png",
+    "oneNavText2": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",
+    "oneNavImg3": "/images/main/simpler/foto-2.png",
+    "oneNavText3": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
+    "oneNavImg4": "/images/main/simpler/foto-1.png",
+    "oneNavText4": "Портрет рисует художник.Детально прорисовывает все части портрета!",
   },
   "three-nav": {
-    "background": "",
-    "oneNavImg1": "",
-    "one-nav__text-1": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",
-    "one-nav_img-2": "",
-    "one-nav__text-2": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
-    "one-nav_img-3": "",
-    "one-nav__text-3": "Портрет рисует художник.Детально прорисовывает все части портрета!",
-    "one-nav_img-4": "",
-    "one-nav__text-4": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем.",  
+    "background": "./images/main/simpler/nav-3.png",
+    "oneNavImg1": "./images/main/simpler/foto-3.png",
+    "oneNavText1": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",
+    "oneNavImg2": "./images/main/simpler/foto-2.png",
+    "oneNavText2": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
+    "oneNavImg3": "./images/main/simpler/foto-1.png",
+    "oneNavText3": "Портрет рисует художник.Детально прорисовывает все части портрета!",
+    "oneNavImg4": "./images/main/simpler/foto-4.png",
+    "oneNavText4": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем.",  
   },
   "four-nav": {
-    "background": "",
-    "oneNavImg1": "",
-    "one-nav__text-1": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
-    "one-nav_img-2": "",
-    "one-nav__text-2": "Портрет рисует художник.Детально прорисовывает все части портрета!",
-    "one-nav_img-3": "",
-    "one-nav__text-3": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем.",
-    "one-nav_img-4": "",
-    "one-nav__text-4": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",   
+    "background": "./images/main/simpler/nav-4.png",
+    "oneNavImg1": "./images/main/simpler/foto-2.png",
+    "oneNavText1": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
+    "oneNavImg2": "./images/main/simpler/foto-1.png",
+    "oneNavText2": "Портрет рисует художник.Детально прорисовывает все части портрета!",
+    "oneNavImg3": "./images/main/simpler/foto-4.png",
+    "oneNavText3": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем.",
+    "oneNavImg4": "./images/main/simpler/foto-3.png",
+    "oneNavText4": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",   
   },
   "five-nav": {
-    "background": "",
-    "oneNavImg1": "",
-    "one-nav__text-1": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",
-    "one-nav_img-2": "",
-    "one-nav__text-2": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
-    "one-nav_img-3": "",
-    "one-nav__text-3": "Портрет рисует художник.Детально прорисовывает все части портрета!",
-    "one-nav_img-4": "",
-    "one-nav__text-4": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем.",  
+    "background": "./images/main/simpler/nav-5.png",
+    "oneNavImg1": "./images/main/simpler/foto-3.png",
+    "oneNavText1": "Яркость и прочность материалов более 100 лет! Холст сохранит свое натяжение и не провиснет!",
+    "oneNavImg2": "./images/main/simpler/foto-4.png",
+    "oneNavText2": "Каждая картина упаковывается подарочную бумагу и украшается бантом!",
+    "oneNavImg3": "./images/main/simpler/foto-2.png",
+    "oneNavText3": "Портрет рисует художник.Детально прорисовывает все части портрета!",
+    "oneNavImg4": "./images/main/simpler/foto-1.png",
+    "oneNavText4": "Покрытие Вашей картины защитным акриловым лаком или текстурным гелем.",  
   },
 }
 
--- js/toChangeTab.js
+++ js/toChangeTab.js
@@ -1,29 +1,41 @@
 import infoList from "./infoList";
 
 export function toChangeTab() {
+  //Функция табов
   let navElements = document.querySelectorAll('.nav-tab');
   let tabImgContainer = document.querySelector('.tab-images__container img');
+  let tabImg_1 = document.querySelector('.tab-img-1 img');
+  let tabImg_2 = document.querySelector('.tab-img-2 img');
+  let tabImg_3 = document.querySelector('.tab-img-3 img');
+  let tabImg_4 = document.querySelector('.tab-img-4 img');
+  let tabText_1 = document.querySelector('.tab-text-1');
+  let tabText_2 = document.querySelector('.tab-text-2');
+  let tabText_3 = document.querySelector('.tab-text-3');
+  let tabText_4 = document.querySelector('.tab-text-4');
   
-
-
-  navElements.forEach(function (navElem) {
-  
+  navElements.forEach(function (navElem) {  
     navElem.addEventListener('click', function (event) {
       let action = event.target;
       disabledNav(navElements)
       action.classList.add('active-nav'); 
 
       let elementNav = infoList[action.dataset.nav];
-      let info = Object.values(elementNav)[0];
-      tabImgContainer.src = info.background;
-      
+      let info = Object.values(elementNav);
+      console.log(info[0]);
+      tabImgContainer.src = info[0];
+      tabImg_1.src = info[1];
+      tabImg_2.src = info[3];
+      tabImg_3.src = info[5];
+      tabImg_4.src = info[7];
+      tabText_1.textContent = info[2];
+      tabText_2.textContent = info[4];
+      tabText_3.textContent = info[6];
+      tabText_4.textContent = info[8];
     });
 
   });
 
-
-
-
+//Сброс стилей у неактивных табов при клике
   function disabledNav(blocks) {
     blocks.forEach(function (el) {
       el.classList.remove('active-nav');
